60da2d03c2af8eda31123d9e356fcc789831e276
beamsoul <nurzhanasamidinova@gmail.com>
2019-07-31 17:24:44 -0700

    git push output

---
+++ sec_group.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "sec1" {
+  name        = "sec1"
+  description = "Allow SSH"
+  vpc_id      = "vpc-093890c9903855ab3"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  
+  tags = {
+        Name = "Dev"
+        Dept =  "IT"
+        Group = "April"
+        Created_by = "janna"
+    }
+}
